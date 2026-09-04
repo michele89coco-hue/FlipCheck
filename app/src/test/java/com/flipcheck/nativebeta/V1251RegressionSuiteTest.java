9719c0281b0a9dfba96c1a3668ad9276b2894ca1
michele89coco-hue <michele89coco@gmail.com>
2026-09-04 12:25:38 +0200

    test: report exact failing regression suite

--- app/src/test/java/com/flipcheck/nativebeta/V1251RegressionSuiteTest.java
+++ app/src/test/java/com/flipcheck/nativebeta/V1251RegressionSuiteTest.java
@@ -6,13 +6,15 @@ import org.junit.Test;
 public final class V1251RegressionSuiteTest {
     @Test
     public void allDeclaredRegressionsPass() throws Exception {
-        V122PhotographicIdentityArchitectureRegressionTest.main(new String[0]);
-        V123CanonicalProfileRegressionTest.main(new String[0]);
-        V124ProductionAliasEndToEndRegressionTest.main(new String[0]);
-        V125HierarchicalRecoveryEndToEndRegressionTest.main(new String[0]);
-        V126SemanticDecisionEndToEndRegressionTest.main(new String[0]);
-        V127CatalogCompatibilityEndToEndRegressionTest.main(new String[0]);
-        V128ExactCatalogResolutionEndToEndRegressionTest.main(new String[0]);
-        V129UniversalCatalogEvidenceRegressionTest.main(new String[0]);
+        run("v122",()->V122PhotographicIdentityArchitectureRegressionTest.main(new String[0]));
+        run("v123",()->V123CanonicalProfileRegressionTest.main(new String[0]));
+        run("v124",()->V124ProductionAliasEndToEndRegressionTest.main(new String[0]));
+        run("v125",()->V125HierarchicalRecoveryEndToEndRegressionTest.main(new String[0]));
+        run("v126",()->V126SemanticDecisionEndToEndRegressionTest.main(new String[0]));
+        run("v127",()->V127CatalogCompatibilityEndToEndRegressionTest.main(new String[0]));
+        run("v128",()->V128ExactCatalogResolutionEndToEndRegressionTest.main(new String[0]));
+        run("v129",()->V129UniversalCatalogEvidenceRegressionTest.main(new String[0]));
     }
+    private static void run(String name,Checked task)throws Exception{try{task.run();}catch(Throwable t){throw new AssertionError(name+": "+t.getMessage(),t);}}
+    private interface Checked{void run()throws Exception;}
 }
