fdffa0bb3e959c7d8ee122f67b7948da4c05ec4e
michele89coco-hue <michele89coco@gmail.com>
2026-09-11 11:03:01 +0200

    Require the added recognition-only Android regression gate

--- legacy26/tests/run-android-ui.sh
+++ legacy26/tests/run-android-ui.sh
@@ -14,4 +14,4 @@ adb install -t -r legacy26/build/outputs/apk/androidTest/release/legacy26-releas
 adb shell pm grant com.flipcheck.beta.legacy26fix android.permission.POST_NOTIFICATIONS
 adb shell am instrument -w -r -e class com.flipcheck.legacy26.AndroidUiRegressionTest,com.flipcheck.legacy26.GoogleDirectRegressionTest,com.flipcheck.legacy26.BackgroundScanRegressionTest com.flipcheck.beta.legacy26fix.test/androidx.test.runner.AndroidJUnitRunner | tee artifact/android-ui-test.txt
 adb pull /sdcard/Android/data/com.flipcheck.beta.legacy26fix/files/ui159 artifact/android-ui
-grep -q 'OK (40 tests)' artifact/android-ui-test.txt
+grep -q 'OK (41 tests)' artifact/android-ui-test.txt
